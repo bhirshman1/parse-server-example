ef50ba4a5acbab9655a59566e40bb6dcb7a1e974
bhirshman1 <bhirshman1@gmail.com>
2017-01-23 23:53:03 -0500

    added push stuff

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  push: {
+    ios: {
+      pfx: '/Users/bhirshman1/Desktop/Apps/TV_Club/Certificates.p12'
+       passphrase: '', // optional password to your p12/PFX
+        bundleId: '',
+        production: false
+      }
+    }
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
