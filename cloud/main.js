5395a41cacbd632e279fd2cb7a8f78ec21513322
bhirshman1 <bhirshman1@gmail.com>
2017-01-25 23:48:20 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,11 +2,11 @@ Parse.Cloud.define("iosPush", function(request, response) {
 
   var user = request.user;
   var params = request.params;
-  var someKey = params.someKey;
+  //var someKey = params.someKey;
   var data = params.data;
 
-  var recipientUser = new Parse.User();
-  recipientUser.id = someKey;
+  //var recipientUser = new Parse.User();
+  //recipientUser.id = someKey;
 
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
