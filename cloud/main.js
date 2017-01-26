9fa2bd6a014b49842b33f2c1b78aee7245d69895
bhirshman1 <bhirshman1@gmail.com>
2017-01-25 21:05:47 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,24 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+// iOS push testing
+Parse.Cloud.define("iosPush", function(request, response) {
+ 
+  var user = request.user;
+  var params = request.params;
+  var someKey = params.someKey
+  var data = params.data
+ 
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
+  pushQuery.equalTo("someKey", someKey)
+ 
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: data
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+ 
+  response.success('success');
 });
+
