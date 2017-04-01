98c53f6a26d9d17a97bbfda590b1f6eda90a03c3
bhirshman1 <bhirshman1@gmail.com>
2017-04-01 00:52:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   push: {
    ios: {
-      pfx: './Certificates.p12',
+      pfx: './DistributionPush.p12',
       bundleId: 'TV-Club',
-      production: false
+      production: true
     }
   },
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
